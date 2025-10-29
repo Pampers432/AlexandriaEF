187b5a3b6229b1349047cc9fb852f2938ba47cab
Pampers432 <max23012007@gmail.com>
2025-10-29 17:41:22 +0300

    Добавил абстракции

---
+++ Alexandria/Abstraction/IAuthorsRepository.cs
@@ -0,0 +1,13 @@
+﻿using AlexandriaEF.Models;
+
+namespace AlexandriaEF.Abstraction
+{
+    public interface IAuthorsRepository
+    {
+        Task<string> AddAuthorAsync(Author newAuthor);
+        Task<string> DeleteAuthorByIdAsync(Author author);
+        Task<Author?> GetAuthorByIdAsync(Guid id);
+        Task<List<Author>> GetAuthors();
+        Task<string> UpdateAuthorByIdAsync(Author author);
+    }
+}
---
+++ Alexandria/Abstraction/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using AlexandriaEF.Contracts;
+
+namespace AlexandriaEF.Abstraction
+{
+    public interface IBookService
+    {
+        Task<string> AddBookAsync(AddBookRequest addBookRequest);
+        Task<string> DeleteBookByIdAsync(BookByTitleRequest bookByTitleRequest);
+        Task<BookResponse> GetBookByTitleAsync(BookByTitleRequest bookByTitleRequest);
+        Task<List<BookResponse>> GetBooksAsync();
+        Task<string> UpdateBookByIdAsync(UpdateBookRequest updateBookRequest);
+    }
+}
